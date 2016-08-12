dd17572b3b3ad5351155b9d41056b7fd230ba492
phalamag <phalamag@gmail.com>
2016-08-12 20:13:49 +0200

    Removed unneeded comments.

--- mysite/mysite/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# 1. Confirm the authentication works.
-# 2. Create an admin user, non-staff user, two ModelBase objects in the tests. One published, the other not.
-# 3. Confirm admin has access to all, non-staff only has access to published.
-# 4. You only need to test retrieval because other API functions are already tested in DRF, DRFE and Jmbo.
